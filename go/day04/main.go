305a85fb4a13629a6be8fc0f3858d6ed99b9d814
hirotake111 <hirotake111@users.noreply.github.com>
2024-12-05 13:56:04 +0900

    pass day04/part2

--- go/day04/main.go
+++ go/day04/main.go
@@ -13,24 +13,64 @@ const (
 )
 
 func main() {
-	input, err := getInput()
+	grid, err := getInput()
 	if err != nil {
 		log.Fatal(err)
 	}
-	// fmt.Println(input)
+	// fmt.Println(grid)
 	var total int
-	for i, row := range input {
+	for i, row := range grid {
 		for j, c := range row {
 			if c == 'X' {
 				// fmt.Printf("Found X: (%d, %d)\n", i, j)
-				total += findXMAS(i, j, input)
+				total += findXMAS(i, j, grid)
 			}
 		}
 	}
 	fmt.Printf("Part1 -> %d\n", total)
+
+	var total2 int
+	for i := 1; i < len(grid)-1; i++ {
+		for j := 1; j < len(grid[0])-1; j++ {
+			if grid[i][j] == 'A' {
+				total2 += findX_MAS(i, j, grid)
+			}
+		}
+	}
+	fmt.Printf("Part2 -> %d\n", total2)
 }
 
 var directions = [8][2]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}}
+
+func findX_MAS(i, j int, grid [][]byte) int {
+	var total int
+	flag := false
+	switch [2]byte{grid[i-1][j-1], grid[i+1][j+1]} {
+	case [2]byte{'M', 'S'}, [2]byte{'S', 'M'}:
+		// p(i, j, grid)
+		flag = true
+	}
+	switch [2]byte{grid[i-1][j+1], grid[i+1][j-1]} {
+	case [2]byte{'M', 'S'}, [2]byte{'S', 'M'}:
+		// p(i, j, grid)
+		flag = flag && true
+	default:
+		flag = false
+	}
+	if flag {
+		// p(i, j, grid)
+		total++
+	}
+	return total
+}
+
+func p(i, j int, grid [][]byte) {
+	fmt.Printf("(%d, %d)\n", i, j)
+	fmt.Printf("%c%c%c\n", grid[i-1][j-1], grid[i-1][j], grid[i-1][j+1])
+	fmt.Printf("%c%c%c\n", grid[i][j-1], grid[i][j], grid[i][j+1])
+	fmt.Printf("%c%c%c\n", grid[i+1][j-1], grid[i+1][j], grid[i+1][j+1])
+	fmt.Println("")
+}
 
 func findXMAS(i, j int, grid [][]byte) int {
 	var total int
