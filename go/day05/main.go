c28a8924207168579241bdfe76d6527b28dca137
hirotake111 <hirotake111@users.noreply.github.com>
2024-12-05 15:59:01 +0900

    go: pass day05 part1

--- go/day05/main.go
+++ go/day05/main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
 const (
 	fileName = "./day05/sample.txt"
+	// fileName = "./day05/input.txt"
 )
 
 func main() {
@@ -16,8 +18,74 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(section1)
-	fmt.Println(section2)
+	// fmt.Println(section1)
+	// fmt.Println(section2)
+	m, err := toMap(section1)
+	// fmt.Printf("m: %+v\n", m)
+	var total, corrected int
+	for _, s := range section2 {
+		arr, err := toIntArr(s)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// fmt.Printf("arr: %v\n", arr)
+		seen := make(map[int]struct{})
+		flag := true
+		for _, n := range arr {
+			if _, ok := seen[n]; ok {
+				flag = false
+				break
+			}
+			if hs, ok := m[n]; ok {
+				for k := range hs {
+					seen[k] = struct{}{}
+				}
+			}
+		}
+		if flag {
+			total += arr[len(arr)/2]
+		} else {
+
+		}
+	}
+	fmt.Printf("Part1 -> %d\n", total)
+	fmt.Printf("Part2 -> %d\n", total+corrected)
+
+}
+
+func toIntArr(s string) ([]int, error) {
+	arr := make([]int, 0)
+	for _, ss := range strings.Split(s, ",") {
+		n, err := strconv.Atoi(ss)
+		if err != nil {
+			return nil, err
+		}
+		arr = append(arr, n)
+	}
+	return arr, nil
+}
+
+func toMap(section []string) (map[int]map[int]struct{}, error) {
+	m := make(map[int]map[int]struct{})
+	for _, s := range section {
+		v := strings.Split(s, "|")
+		l, err := strconv.Atoi(v[0])
+		if err != nil {
+			return nil, err
+		}
+		r, err := strconv.Atoi(v[1])
+		if err != nil {
+			return nil, err
+		}
+		if hs, ok := m[r]; !ok {
+			hs := make(map[int]struct{}, 0)
+			hs[l] = struct{}{}
+			m[r] = hs
+		} else {
+			hs[l] = struct{}{}
+		}
+	}
+	return m, nil
 }
 
 func getData() ([]string, []string, error) {
