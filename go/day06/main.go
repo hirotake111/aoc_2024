451815e259a24528e152d0b5083743b2e9c4711b
hirotake111 <hirotake111@users.noreply.github.com>
2024-12-06 16:16:08 +0900

    go: pass day06 part2

--- go/day06/main.go
+++ go/day06/main.go
@@ -24,29 +24,97 @@ func main() {
 	if v := part1(example); v != 41 {
 		log.Fatalf("part1 failed: %v != 41", v)
 	}
+	if v := part2(example); v != 6 {
+		log.Fatalf("part2 failed: %v != 6", v)
+	}
 	rawData, err := os.ReadFile("./data/day06.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("Part1 -> %d\n", part1(string(rawData)))
+	fmt.Printf("Part2 -> %d\n", part2(string(rawData)))
+}
+
+func part2(s string) int {
+	grid := stringToGrid(s)
+	r, c := find_guard(grid)
+	paths := collectPath(grid, r, c)[1:]
+	format(grid)
+	var options int
+	for _, p := range paths {
+		if canMakeLoop(grid, r, c, p[0], p[1]) {
+			options++
+		}
+	}
+	return options
+}
+
+func format(grid [][]byte) {
+	for i, row := range grid {
+		for j, c := range row {
+			if c == '.' {
+				grid[i][j] = 0
+			} else {
+				grid[i][j] = 16
+			}
+		}
+	}
+}
+
+func canMakeLoop(src [][]byte, r, c, a, b int) bool {
+	grid := make([][]byte, 0, len(src))
+	for _, srcRow := range src {
+		row := make([]byte, len(srcRow))
+		copy(row, srcRow)
+		grid = append(grid, row)
+	}
+	grid[r][c] = 0
+	grid[a][b] = 16
+	m, n := len(grid), len(grid[0])
+	for {
+		// 1: up, 2: right, 4: down, 8: left
+		for _, delta := range [4][3]int{{-1, 0, 1}, {0, 1, 2}, {1, 0, 4}, {0, -1, 8}} {
+			for {
+				nr, nc := r+delta[0], c+delta[1]
+				if nr < 0 || nr >= m || nc < 0 || nc >= n {
+					// Out of the grid
+					return false
+				}
+				if grid[nr][nc] == 16 { // Change the direction
+					break
+				}
+				r, c = nr, nc
+				bit := byte(delta[2])
+				if grid[r][c]&bit == bit {
+					// Found the loop!
+					return true
+				} else {
+					grid[r][c] = grid[r][c] | bit
+				}
+
+			}
+		}
+	}
 }
 
 func part1(s string) int {
 	grid := stringToGrid(s)
-	paths := collectPath(grid)
+	r, c := find_guard(grid)
+	paths := collectPath(grid, r, c)
 	return len(paths)
 }
 
-func collectPath(src [][]byte) [][2]int {
-	grid := make([][]byte, len(src))
-	copy(grid, src)
-	// p(grid)
+func collectPath(src [][]byte, r, c int) [][2]int {
+	grid := make([][]byte, 0, len(src))
+	for _, srcRow := range src {
+		row := make([]byte, len(srcRow))
+		copy(row, srcRow)
+		grid = append(grid, row)
+	}
 	m, n := len(grid), len(grid[0])
-	r, c := find_guard(grid)
 	grid[r][c] = 'X'
-	// fmt.Printf("x: %d, y: %d\n", x, y)
 	var paths [][2]int = [][2]int{{r, c}}
-	for r >= 0 && r < m && c >= 0 && c < n {
+	for {
 		for _, delta := range [4][2]int{{-1, 0}, {0, 1}, {1, 0}, {0, -1}} {
 			for {
 				nr, nc := r+delta[0], c+delta[1]
@@ -66,7 +134,6 @@ func collectPath(src [][]byte) [][2]int {
 			}
 		}
 	}
-	panic("unreachable")
 }
 
 func stringToGrid(s string) [][]byte {
@@ -91,7 +158,11 @@ func find_guard(grid [][]byte) (int, int) {
 
 func p(grid [][]byte) {
 	for _, row := range grid {
-		fmt.Println(string(row))
+		// fmt.Println(row)
+		for _, c := range row {
+			fmt.Printf("%02d ", c)
+		}
+		fmt.Println("")
 	}
 	fmt.Println("")
 }
